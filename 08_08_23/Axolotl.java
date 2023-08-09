d2996705bc7fdcdfdd98ed2bf83a8076da75297e
ECaratacheaF22 <120129178+ECaratacheaF22@users.noreply.github.com>
2023-08-09 15:30:58 +0000

    pushing

---
+++ 08_08_23/Axolotl.java
@@ -0,0 +1,21 @@
+public class Axolotl {
+    public static void main(String[] args) {
+        System.out.println(" \\   ________   /");
+        System.out.println("  \\ /        \\/  ");
+        System.out.println(" \\_|  O   O   |_/");
+        System.out.println(" __|          |__");
+        System.out.println("    \\________/");
+        System.out.println("   ___|   |___ ");
+        System.out.println("  {___|   |___} ");
+        System.out.println("      |   |    ");
+        System.out.println("     /     \\    ");
+        System.out.println("    / /\\ /\\ \\   ");
+        System.out.println("    ww | |  ww ");
+        System.out.println("       \\/     ");
+        
+
+
+
+        
+    }
+}
---
+++ 08_08_23/Drawing.java
@@ -0,0 +1,12 @@
+public class Drawing {
+    public static void main(String[] args) {
+        System.out.println("    oo oo    ");
+        System.out.println("  o       o  ");
+        System.out.println(" o  o   o  o");
+        System.out.println(" o         o");
+        System.out.println(" o  o   o  o");
+        System.out.println("  o  o o  o");
+        System.out.println("    oo oo  ");
+        
+    }
+}
